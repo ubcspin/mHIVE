1a539044107d360e6844253dd35d42f74b1de1bc
Oliver Schneider <oli@host186-225.wifi.ubc.ca>
2013-06-19 13:30:24 -0700

    Removed SystemUIHider.
    We don't need to hide the System UI for our app. This cleaned up the code substantially.

--- mHIVE/src/org/spin/mhive/MainActivity.java
+++ mHIVE/src/org/spin/mhive/MainActivity.java
@@ -1,10 +1,7 @@
 package org.spin.mhive;
 
-import org.spin.mhive.util.SystemUiHider;
-
 import com.example.mhive.R;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.graphics.Point;
 import android.media.AudioFormat;
@@ -29,18 +26,7 @@ import android.widget.TextView;
  */
 public class MainActivity extends Activity {
 
-    /**
-     * The flags to pass to {@link SystemUiHider#getInstance}.
-     */
-    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
-
-    /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
-    private SystemUiHider mSystemUiHider;
-    
-    
-    private int screenHeight;
+	private int screenHeight;
     private int screenWidth;
     private int minFreq = 20;
     private int maxFreq = 140;
@@ -53,68 +39,11 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
         
-        
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
         screenWidth = size.x;
         screenHeight = size.y;
-        
-        
-
-        final View contentView = findViewById(R.id.fullscreen_content);
-
-        // Set up an instance of SystemUiHider to control the system UI for
-        // this activity.
-        mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
-        mSystemUiHider.setup();
-        mSystemUiHider
-                .setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-                    // Cached values.
-                    int mControlsHeight;
-                    int mShortAnimTime;
-
-                    @Override
-                    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-                    public void onVisibilityChange(boolean visible) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-                            // If the ViewPropertyAnimator API is available
-                            // (Honeycomb MR2 and later), use it to animate the
-                            // in-layout UI controls at the bottom of the
-                            // screen.
-//                            if (mControlsHeight == 0) {
-//                                mControlsHeight = controlsView.getHeight();
-//                            }
-                            if (mShortAnimTime == 0) {
-                                mShortAnimTime = getResources().getInteger(
-                                        android.R.integer.config_shortAnimTime);
-                            }
-//                            controlsView.animate()
-//                                    .translationY(visible ? 0 : mControlsHeight)
-//                                    .setDuration(mShortAnimTime);
-                        } 
-//                        else {
-                            // If the ViewPropertyAnimator APIs aren't
-                            // available, simply show or hide the in-layout UI
-                            // controls.
-//                            controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
-//                        }
-
-                    }
-                });
-        
-        /*controlsView.setOnTouchListener(new OnTouchListener(){
-
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				TextView t = (TextView)findViewById(R.id.testText1);
-				t.setText(""+event.getX());
-				return false;
-			}
-        	
-        	
-        });*/
-        
     }
 
     @Override
@@ -135,17 +64,5 @@ public class MainActivity extends Activity {
     	}
 
     	return false;
-    }
-    
-    
-
-    Handler mHideHandler = new Handler();
-    Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            mSystemUiHider.hide();
-        }
-    };
-          
-    
+    }          
 }
