d12fe726c7f4eb8bf3f2bf0dbf851c6ac5376f7c
Oliver Schneider <oli@host179-229.wifi.ubc.ca>
2013-06-20 14:01:15 -0700

    Added layout for Waveform Dialog

--- mHIVE/src/org/spin/mhive/WaveformDialog.java
+++ mHIVE/src/org/spin/mhive/WaveformDialog.java
@@ -1,7 +1,25 @@
 package org.spin.mhive;
 
-import android.support.v4.app.DialogFragment;
+import com.example.mhive.R;
 
-public class WaveformDialog extends DialogFragment {
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WaveformDialog extends DialogFragment
+{
+	public WaveformDialog()
+	{
+//        setContentView(R.layout.activity_waveform_selection);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		View view = inflater.inflate(R.layout.activity_waveform_selection, container);
+		return view;
+	}
 
 }
