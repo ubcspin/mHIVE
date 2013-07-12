2a1240ca13244ba06e70be3a629287cf8c3fbaec
Oliver Schneider <oli@oliver-schneiders-macbook-pro-15.local>
2013-07-12 10:22:49 -0700

    ADSRVis: Added stub circles.

--- mHIVE/src/org/spin/mhive/ADSRView.java
+++ mHIVE/src/org/spin/mhive/ADSRView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -11,12 +13,20 @@ public class ADSRView extends View {
 	
 	Paint linePaint;
 	Paint bgPaint;
+	Paint circleStrokePaint;
+	Paint circleBGPaint;
 
+	RectF attackDecayCircle;
+	RectF decaySustainCircle;
+	RectF sustainReleaseCircle;
 	
 	float attack, decay, sustain, release;
 	
 	private final float MS_IN_WIDTH = 1000;
 	private final int nDottedLinesForSustain = 10;
+	
+	
+	private final int SELECTION_CIRCLE_RADIUS = 35;
 	
 	public ADSRView(Context context) {
 		super(context);
@@ -43,10 +53,20 @@ public class ADSRView extends View {
 		bgPaint = new Paint();
 		bgPaint.setColor(Color.WHITE);
 		
+		circleStrokePaint = new Paint();
+		circleStrokePaint.setARGB(255, 0, 0, 255);
+		circleStrokePaint.setStrokeWidth(5);
+		circleStrokePaint.setStyle(Style.STROKE);
+		
+		circleBGPaint = new Paint();
+		circleBGPaint.setARGB(127, 0, 0, 255);
+		circleBGPaint.setStyle(Style.FILL);
+		
 		attack = 100;
 		decay = 100;
 		sustain = 0.5f;
 		release = 100;
+		
 	}
 	
 	private float MS2Width(float ms)
@@ -78,6 +98,7 @@ public class ADSRView extends View {
 	@Override
 	public void onDraw(Canvas c)
 	{
+		//bg
 		c.drawRect(0, 0, getWidth(), getHeight(), bgPaint);
 		
 		//attack
@@ -100,6 +121,23 @@ public class ADSRView extends View {
 		
 		//release
 		c.drawLine(SustainRight(), SustainHeight(), getWidth(), getHeight(), linePaint);
+		
+		
+		//TODO: Move this to UPDATE
+		attackDecayCircle = new RectF(	MS2Width(attack)-SELECTION_CIRCLE_RADIUS, -SELECTION_CIRCLE_RADIUS,
+										MS2Width(attack)+SELECTION_CIRCLE_RADIUS, SELECTION_CIRCLE_RADIUS);
+		decaySustainCircle = new RectF(	SustainLeft()-SELECTION_CIRCLE_RADIUS, SustainHeight()-SELECTION_CIRCLE_RADIUS,
+										SustainLeft()+SELECTION_CIRCLE_RADIUS, SustainHeight()+SELECTION_CIRCLE_RADIUS);
+		sustainReleaseCircle = new RectF(	SustainRight()-SELECTION_CIRCLE_RADIUS, SustainHeight()-SELECTION_CIRCLE_RADIUS,
+											SustainRight()+SELECTION_CIRCLE_RADIUS, SustainHeight()+SELECTION_CIRCLE_RADIUS);
+		
+		//selection circles
+		c.drawOval(attackDecayCircle, circleBGPaint);
+		c.drawOval(attackDecayCircle, circleStrokePaint);
+		c.drawOval(decaySustainCircle, circleBGPaint);
+		c.drawOval(decaySustainCircle, circleStrokePaint);
+		c.drawOval(sustainReleaseCircle, circleBGPaint);
+		c.drawOval(sustainReleaseCircle, circleStrokePaint);
 	}
 
 }
