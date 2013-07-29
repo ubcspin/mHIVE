adea9e25ceb273e9d3874c7c749c16368aa5ba0d
Oliver Schneider <oli@vpn33.cs.ubc.ca>
2013-07-29 15:19:54 -0700

    SettingsDialog: Added minimum, maximum, and default values to number pickers for frequncy range manipulation

--- mHIVE/src/org/spin/mhive/SettingsDialog.java
+++ mHIVE/src/org/spin/mhive/SettingsDialog.java
@@ -1,15 +1,21 @@
 package org.spin.mhive;
 
 import android.app.DialogFragment;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
 
 import com.example.mhive.R;
 
 public class SettingsDialog extends DialogFragment
 {	
+	NumberPicker minFrequencyPicker;
+	NumberPicker maxFrequencyPicker;
+	
+	MainActivity parent;
 	
 	public SettingsDialog()
 	{
@@ -20,6 +26,21 @@ public class SettingsDialog extends DialogFragment
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
 		View view = inflater.inflate(R.layout.settings_dialog_layout, container);
+		
+		minFrequencyPicker = (NumberPicker)view.findViewById(R.id.nmbrMinimumFrequency);
+		maxFrequencyPicker = (NumberPicker)view.findViewById(R.id.nmbrMaximumFrequency);
+		
+		parent = (MainActivity)getActivity();
+		
+		Resources res = getResources();
+		minFrequencyPicker.setMinValue(res.getInteger(R.integer.MinAllowedFrequencyInHz));
+		minFrequencyPicker.setMaxValue(res.getInteger(R.integer.MaxAllowedFrequencyInHz));
+
+		maxFrequencyPicker.setMinValue(res.getInteger(R.integer.MinAllowedFrequencyInHz));
+		maxFrequencyPicker.setMaxValue(res.getInteger(R.integer.MaxAllowedFrequencyInHz));
+
+		minFrequencyPicker.setValue(parent.getMinFreq());
+		maxFrequencyPicker.setValue(parent.getMaxFreq());
 		
 		return view;
 	}
